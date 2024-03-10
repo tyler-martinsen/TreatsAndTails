6d04ee11e159afd262a3828680b4613b10335789
tyler-martinsen <tylermartinsen@gmail.com>
2024-03-09 23:20:15 -0700

    Styling Fixes

---
+++ Components/Services/InventoryService.cs
@@ -0,0 +1,20 @@
+﻿using TreatsAndTails.Models;
+
+namespace TreatsAndTails.Components.Services
+{
+    public class InventoryService
+    {
+
+        private readonly TatContext _context;
+
+        public InventoryService(TatContext context)
+        {
+            this._context = context;
+        }
+
+        public List<Product> GetInventory()
+        {
+            return _context.Products.ToList();
+        }
+    }
+}
---
+++ Models/_User.cs
@@ -0,0 +1,17 @@
+﻿namespace TreatsAndTails.Models
+{
+	public class _User
+	{
+		public string Email { get; set; } = null!;
+
+		public string Password { get; set; } = null!;
+
+		public string PasswordConfirmed { get; set; } = null!;
+
+		public string FirstName { get; set; } = null!;
+
+		public string LastName { get; set; } = null!;
+
+		public string? PhoneNumber { get; set; }
+	}
+}
