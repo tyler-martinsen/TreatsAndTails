5da8a2135359b02fe26348f1c84de25f08dd947e
tyler-martinsen <tylermartinsen@gmail.com>
2024-04-28 21:19:52 -0600

    Initial Work on Edit Product Structure

--- Components/Services/InventoryService.cs
+++ Components/Services/InventoryService.cs
@@ -17,5 +17,84 @@ namespace TreatsAndTails.Components.Services
         {
             return await _context.Products.ToListAsync();
         }
+
+        public async Task<Product?> GetItem(int id)
+        {
+			return await _context.Products.FirstOrDefaultAsync(item => item.Id == id);
+		}
+
+        public async Task<bool> SetItem(string? shape,string? flavor,string? size,string? description,decimal? cost,int? quantity, int id = 0)
+        {
+            Product newItem = new Product();
+
+            // Add product instead of edit
+            if(id > 0)
+            {
+				newItem = (await _context.Products.FirstOrDefaultAsync(item => item.Id == id)) ?? newItem;
+			}
+
+			// Update Non-null items
+			if (shape != null)
+			{
+				newItem.InvShape = shape;
+			}
+			if (flavor != null)
+			{
+				newItem.InvFlavor = flavor;
+			}
+			if (size != null)
+			{
+				newItem.InvSize = size;
+			}
+			if (description != null)
+			{
+				newItem.InvDescription = description;
+			}
+			if (cost != null)
+			{
+				newItem.Cost = cost ?? newItem.Cost;
+			}
+			if (quantity != null)
+			{
+				newItem.Quantity = quantity ?? newItem.Quantity;
+			}
+
+			// Ensure All Fields Are Valid
+			if(newItem == null)
+			{
+				return false;
+			}
+			else if (newItem.InvShape == null)
+			{
+				return false;
+			}
+			else if (newItem.InvFlavor == null)
+			{
+				return false;
+			}
+			else if (newItem.InvSize == null)
+			{
+				return false;
+			}
+			else if (newItem.InvDescription == null)
+			{
+				return false;
+			}
+			else if (newItem.Cost < 0)
+			{
+				return false;
+			}
+			else if (newItem.Quantity < 0)
+			{
+				return false;
+			}
+
+			if (id <= 0)
+			{
+				await _context.Products.AddAsync(newItem);
+			}
+
+			return await _context.SaveChangesAsync() > 0;
+		}
     }
 }
